45b3af0c71c927f7dcdfdae88ca3f8d98c2c887e
Vinay Vyas <32808187+27vinayvyas@users.noreply.github.com>
2020-05-27 23:12:59 +0530

    chore: SharedPref added - Closes#8 (#9)
    * fetch
    
    * feat(STCK-4): stateless and stateful widgets added
    
    * feat(STCK-5): Adds model class
    
    * feat(STCK-5): models/index.dart updated
    
    * feat(STCK-8): SharedPreference Added
    
    * feat(STCK-7): updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,27 @@ import 'package:provider/provider.dart';
 import './shared/providers/name_notifier.dart';
 import './routes.dart';
 import './modules/auth/auth_screen.dart';
+import './shared/data/sharedPreference/sharedPreference.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => ClassName()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        routes: Routes.routes,
-        home: AuthScreen(),
-      ),
-    );
+        providers: [
+          ChangeNotifierProvider(create: (context) => ClassName()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          routes: Routes.routes,
+          home: AuthScreen(),
+        ));
   }
 }

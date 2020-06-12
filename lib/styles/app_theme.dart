cad419eaef131fffc0125d334d82643943002e4d
Vinay Vyas <32808187+27vinayvyas@users.noreply.github.com>
2020-06-12 17:29:43 +0530

    feat: Themes added Closes#15 (#16)
    * fetch
    
    * feat(STCK-4): stateless and stateful widgets added
    
    * feat(STCK-5): Adds model class
    
    * feat(STCK-5): models/index.dart updated
    
    * feat(STCK-8): SharedPreference Added
    
    * feat(STCK-7): updated
    
    * feat(stck-15): Themes added
    
    * feat(stck-15): main.dart changed

--- lib/styles/app_theme.dart
+++ lib/styles/app_theme.dart
@@ -1,3 +1,45 @@
-class AppTheme {
-  AppTheme._();
+import 'package:flutter/material.dart';
+
+ThemeData AppTheme1() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+      //textTheme: _basicTextTheme(base.textTheme),
+      //textTheme: Typography().white,
+      primaryColor: Color(0xffce107c),
+      //primaryColor: Color(0xff4829b2),
+      indicatorColor: Color(0xFF807A6B),
+      scaffoldBackgroundColor: Color(0xFFF5F5F5),
+      accentColor: Color(0xFFFFF8E1),
+      iconTheme: IconThemeData(
+        color: Colors.white,
+        size: 20.0,
+      ),
+      buttonColor: Colors.white,
+      backgroundColor: Colors.white,
+      tabBarTheme: base.tabBarTheme.copyWith(
+        labelColor: Color(0xffce107c),
+        unselectedLabelColor: Colors.grey,
+      ));
+}
+
+ThemeData AppTheme2() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+      //textTheme: _basicTextTheme(base.textTheme),
+      //textTheme: Typography().white,
+      primaryColor: Color(0xffce107c),
+      //primaryColor: Color(0xff4829b2),
+      indicatorColor: Color(0xFF807A6B),
+      scaffoldBackgroundColor: Color(0xFFF5F5F5),
+      accentColor: Color(0xFFFFF8E1),
+      iconTheme: IconThemeData(
+        color: Colors.white,
+        size: 20.0,
+      ),
+      buttonColor: Colors.white,
+      backgroundColor: Colors.white,
+      tabBarTheme: base.tabBarTheme.copyWith(
+        labelColor: Color(0xffce107c),
+        unselectedLabelColor: Colors.grey,
+      ));
 }

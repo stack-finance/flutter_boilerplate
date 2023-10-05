7581dcd9ee516d38c0d9d09b8c6b9ca8dc352993
Yash <pauranik51@gmail.com>
2023-10-05 15:23:23 +0530

    chore: Added home module boilerplate

---
+++ lib/modules/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+    );
+  }
+}

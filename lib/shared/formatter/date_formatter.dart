7819c9ceb4502894b3fd944f6598c6b6897727c5
Yash <pauranik51@gmail.com>
2023-10-05 15:39:07 +0530

    improve: Optimize date formatter

--- lib/shared/formatter/date_formatter.dart
+++ lib/shared/formatter/date_formatter.dart
@@ -1,89 +1,57 @@
 import 'package:flutter/services.dart';
 
 class DateFormatter extends TextInputFormatter {
+  static const int maxDays = 31;
+  static const int maxMonths = 12;
+  static const int maxYear1 = 2;
+  static const int minYear2 = 19;
+  static const int maxYear2 = 20;
+
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
-    int selectionIndex;
-
-    // Get the previous and current input strings
-    var pText = oldValue.text;
-    var cText = newValue.text;
-    // Abbreviate lengths
-    var cLen = cText.length;
-    var pLen = pText.length;
+    String cText = newValue.text;
+    int cLen = cText.length;
 
     if (cLen == 1) {
-      // Can only be 0, 1, 2 or 3
-      if ((int.tryParse(cText) ?? 4) > 3) {
-        // Remove char
+      int firstDigit = int.tryParse(cText) ?? 4;
+      if (firstDigit > 3) {
         cText = '';
       }
-    } else if (cLen == 2 && pLen == 1) {
-      // Days cannot be greater than 31
-      var dd = int.tryParse(cText.substring(0, 2)) ?? 32;
-      if (dd == 0 || dd > 31) {
-        // Remove char
+    } else if (cLen == 2) {
+      int dd = int.tryParse(cText.substring(0, 2)) ?? (maxDays + 1);
+      if (dd == 0 || dd > maxDays) {
         cText = cText.substring(0, 1);
       } else {
-        // Add a / char
         cText += '/';
       }
     } else if (cLen == 4) {
-      // Can only be 0 or 1
-      if ((int.tryParse(cText.substring(3, 4)) ?? 2) > 1) {
-        // Remove char
+      int lastDigit = int.tryParse(cText.substring(3, 4)) ?? (maxYear1 + 1);
+      if (lastDigit > maxYear1) {
         cText = cText.substring(0, 3);
       }
-    } else if (cLen == 5 && pLen == 4) {
-      // Month cannot be greater than 12
-      var mm = int.tryParse(cText.substring(3, 5)) ?? 13;
-      if (mm == 0 || mm > 12) {
-        // Remove char
+    } else if (cLen == 5) {
+      int mm = int.tryParse(cText.substring(3, 5)) ?? (maxMonths + 1);
+      if (mm == 0 || mm > maxMonths) {
         cText = cText.substring(0, 4);
       } else {
-        // Add a / char
         cText += '/';
       }
-    } else if ((cLen == 3 && pLen == 4) || (cLen == 6 && pLen == 7)) {
-      // Remove / char
+    } else if (cLen == 3 || cLen == 6) {
       cText = cText.substring(0, cText.length - 1);
-    } else if (cLen == 3 && pLen == 2) {
-      if ((int.tryParse(cText.substring(2, 3)) ?? 2) > 1) {
-        // Replace char
-        cText = cText.substring(0, 2) + '/';
-      } else {
-        // Insert / char
-        cText =
-            cText.substring(0, pLen) + '/' + cText.substring(pLen, pLen + 1);
-      }
-    } else if (cLen == 6 && pLen == 5) {
-      // Can only be 1 or 2 - if so insert a / char
-      var y1 = int.tryParse(cText.substring(5, 6)) ?? 0;
-      if (y1 < 1 || y1 > 2) {
-        // Replace char
-        cText = cText.substring(0, 5) + '/';
-      } else {
-        // Insert / char
-        cText = cText.substring(0, 5) + '/' + cText.substring(5, 6);
-      }
     } else if (cLen == 7) {
-      // Can only be 1 or 2
-      var y1 = int.tryParse(cText.substring(6, 7)) ?? 0;
-      if (y1 < 1 || y1 > 2) {
-        // Remove char
+      int y1 = int.tryParse(cText.substring(6, 7)) ?? (maxYear1 + 1);
+      if (y1 < 1 || y1 > maxYear1) {
         cText = cText.substring(0, 6);
       }
     } else if (cLen == 8) {
-      // Can only be 19 or 20
-      var y2 = int.tryParse(cText.substring(6, 8)) ?? 18;
-      if (y2 < 19 || y2 > 20) {
-        // Remove char
+      int y2 = int.tryParse(cText.substring(6, 8)) ?? (minYear2 - 1);
+      if (y2 < minYear2 || y2 > maxYear2) {
         cText = cText.substring(0, 7);
       }
     }
 
-    selectionIndex = cText.length;
+    int selectionIndex = cText.length;
     return TextEditingValue(
       text: cText,
       selection: TextSelection.collapsed(offset: selectionIndex),
